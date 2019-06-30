6fcac993910ec18661322c6e67d22f40c753d190
canxiangzhao <canxiang.zhao@seek4paws.com>
2019-06-30 22:31:57 +0800

    finished templates

--- server.py
+++ server.py
@@ -1,15 +1,32 @@
-from flask import Flask 
+from flask import Flask, render_template 
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+from datetime import datetime
 
 app = Flask(__name__)
+manager = Manager(app)
+Bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello zhaocanxiang</h1>'
+    return render_template("index.html", current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def suer(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template("user.html", name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    manager.run()

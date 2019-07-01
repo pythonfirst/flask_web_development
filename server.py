557b075ea3da3d0d72e063ef7c2cc9a8bc246356
canxiangzhao <canxiang.zhao@seek4paws.com>
2019-07-01 23:27:48 +0800

    finish form

--- server.py
+++ server.py
@@ -1,19 +1,39 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
 
 from datetime import datetime
 
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(FlaskForm):
+    name = StringField("What's you name?", validators=[Required()])
+    submit = SubmitField("submit")
+
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "ceshi123456"
+
 manager = Manager(app)
 Bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("index.html", current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("你已经更换了用户名。")
+
+        session["name"] = form.name.data 
+        return redirect(url_for("index"))
+    return render_template("index.html", current_time=datetime.utcnow(), form=form, name=session.get("name"))
 
 @app.route('/user/<name>')
 def suer(name):

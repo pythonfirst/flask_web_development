b623f1dd612ed46e11ef0385608c10a854d36547
canxiangzhao <canxiang.zhao@seek4paws.com>
2019-07-14 23:22:27 +0800

    finish particial sql

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 from datetime import datetime
 
@@ -13,9 +15,17 @@ class NameForm(FlaskForm):
     name = StringField("What's you name?", validators=[Required()])
     submit = SubmitField("submit")
 
+basedir = os.path.abspath(os.path.dirname(__file__))  # 设置数据库用
+
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "ceshi123456"
+
+# 配置数据库
+app.config["SQLALCHEMY_DATABASE_URI"] = \
+    "sqlite:///" + os.path.join(basedir, "data.sqlite")
+app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+db = SQLAlchemy(app)
 
 manager = Manager(app)
 Bootstrap = Bootstrap(app)
@@ -27,13 +37,21 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get("name")
-        if old_name is not None and old_name != form.name.data:
-            flash("你已经更换了用户名。")
-
+        user = User.query.filter_by(username=form.name.data).first()
+        print("user query", user)
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            print("user", user)
+            session["known"] = False
+        else:
+            session["known"] = True
+        
         session["name"] = form.name.data 
+        form.name.data = ""
         return redirect(url_for("index"))
-    return render_template("index.html", current_time=datetime.utcnow(), form=form, name=session.get("name"))
+    return render_template("index.html", current_time=datetime.utcnow(), form=form, \
+        name=session.get("name"), known=session.get("known", False))
 
 @app.route('/user/<name>')
 def suer(name):
@@ -46,6 +64,30 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
+
+
+# 定义数据库模型
+class Role(db.Model):
+    """角色模型"""
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String(64), unique=True)
+
+    users = db.relationship("User", backref="role")
+ 
+    def __repr__(self):
+        return "<Role %r>" % self.name
+
+class User(db.Model):
+    """用户模型"""
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True) 
+    username = db.Column(db.String(64), unique=True, index=True)
+ 
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+    def __repr__(self):
+        return "User %r>" % self.username
+    
 
 if __name__ == "__main__":
     # app.run(debug=True)
